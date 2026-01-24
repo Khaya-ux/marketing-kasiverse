20554e4e2275a1765a8abd918975897e17266bf6
mellow874 <felicitymncube49@gmail.com>
2026-01-24 17:31:40 +0200

    added routing for the home component

--- Kasiverse/src/App.jsx
+++ Kasiverse/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Import pages
 import Problems from "./pages/Problems";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Default route */}
-        <Route path="/" element={<Problems />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/problems" element={<Problems />} />
       </Routes>
     </BrowserRouter>
   );
